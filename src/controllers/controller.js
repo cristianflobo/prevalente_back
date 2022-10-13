e623f0df045d1974049617f8113d0663ab945cf4
cristian lobo <cristianflobo@hotmail.com>
2022-10-12 21:02:53 -0500

    finish end

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,57 +1,58 @@
-const { empresa} = require('../db')
-const ver = require('../db');
-const  dataEmpresa  = require('../utils/dataEmpresa');
+const { empresa } = require("../db");
+const ver = require("../db");
+const dataEmpresa = require("../utils/dataEmpresa");
 
-const getEmpresa = async (req,res) => {
-  let allEmpresa = []
+const getEmpresa = async (req, res) => {
+  let allEmpresa = [];
   try {
-    const validateExisteEmpresa = await empresa.findOne({ where: { name: "Prevalent" } })
-    console.log(validateExisteEmpresa)
+    const validateExisteEmpresa = await empresa.findOne({
+      where: { name: "Prevalent" },
+    });
     if (validateExisteEmpresa === null) {
-      await Promise.all (dataEmpresa.map( async(item) => {
-        await empresa.create({
-          name:item.name,
-          razon:item.razon,
-          nit:item.nit,
-          identificacion:item.identificacion,
-          empleados:item.empleados,
-          logo:item.logo,
-          aprobada:item.aprobada
-        })  
-      }))
+      await Promise.all(
+        dataEmpresa.map(async (item) => {
+          await empresa.create({
+            name: item.name,
+            razon: item.razon,
+            nit: item.nit,
+            identificacion: item.identificacion,
+            empleados: item.empleados,
+            logo: item.logo,
+            aprobada: item.aprobada,
+          });
+        })
+      );
     }
-    allEmpresa = await empresa.findAll()
-    res.send(allEmpresa).end()
+    allEmpresa = await empresa.findAll();
+    res.send(allEmpresa).end();
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-        
-}
+};
 const edit = async (req, res) => {
-    console.log(typeof req.body)
-    const {name, aprobada } = req.body;
-    try {
-      const validateExisteEmpresa = await empresa.findOne({ where: { name: name } })
-      console.log(validateExisteEmpresa.dataValues.id)
-      const update =  await empresa.upsert({
-        id: validateExisteEmpresa.dataValues.id,
-        name,
-        razon: validateExisteEmpresa.dataValues.razon,
-        nit: validateExisteEmpresa.dataValues.nit,
-        identificacion: validateExisteEmpresa.dataValues.identificacion,
-        empleados: validateExisteEmpresa.dataValues.empleados,
-        logo: '',
-        aprobada: aprobada
-      });
-      res.send(update)
-    } catch (error) {
-      console.log(error)
-    }
-      res.end()
-   
-  };
+  const { name, aprobada } = req.body;
+  try {
+    const validateExisteEmpresa = await empresa.findOne({
+      where: { name: name },
+    });
+    const update = await empresa.upsert({
+      id: validateExisteEmpresa.dataValues.id,
+      name,
+      razon: validateExisteEmpresa.dataValues.razon,
+      nit: validateExisteEmpresa.dataValues.nit,
+      identificacion: validateExisteEmpresa.dataValues.identificacion,
+      empleados: validateExisteEmpresa.dataValues.empleados,
+      logo: "",
+      aprobada: aprobada,
+    });
+    res.send(update);
+  } catch (error) {
+    console.log(error);
+  }
+  res.end();
+};
 
 module.exports = {
   getEmpresa,
   edit,
-}
+};
